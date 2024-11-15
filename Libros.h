a581ffcb30cfcaea6293f1b80e27154bdd6fc9a6
Isabel Vaca <113130012+IsabelVaca@users.noreply.github.com>
2024-11-15 02:59:10 -0600

    Add files via upload

--- Libros.h
+++ Libros.h
@@ -1,38 +1,46 @@
 //
-//  Libros.h
-//  Biblioteca personal corr
+//  Libro.h
+//  Projecto biblioteca prueba
 //
-//  Created by Isabel Vaca on 08/11/24.
+//  Created by Isabel Vaca on 14/11/24.
 //
 
-#ifndef LIBROS_H
-#define LIBROS_H
 
 #include <string>
 #include <iostream>
 
 using namespace std;
-
+ 
 class Libro {
 private:
     string titulo;
     string genero;
-
+    
 public:
-    Libro(string t, string g): titulo(t), genero(g) {}
-
-    string get_titulo(){
-        return titulo;
-    }
-
-    string get_genero(){
-        return genero;
-    }
-
+    Libro(string t, string g): titulo(t), genero(g){}
+    
+    string get_titulo();
+    string get_genero();
+    
+    void set_titulo(string t);
+    void set_genero(string g);
+    
     void mostrar_libro(){
-        cout << "Título: " << titulo << endl;
-        cout << "Género: " << genero << endl;
+        cout<<" Titulo: " << titulo<< endl;
+        cout<<"Genero: " << genero << endl;
     }
+    
 };
-
-#endif 
+string Libro::get_titulo(){
+    return titulo;
+}
+string Libro::get_genero(){
+    return genero;
+}
+void Libro::set_titulo(string t){
+    titulo = t;
+}
+void Libro::set_genero(string g){
+    genero = g;
+}
+    
