2f2cd17f9eb63fc4d0ce7b7a30966cbca4f932cc
Isabel Vaca <113130012+IsabelVaca@users.noreply.github.com>
2024-11-22 03:06:03 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,45 @@
 //
 //  main.cpp
-//  Projecto biblioteca prueba
+//  Biblioteca personal
 //
-//  Created by Isabel Vaca on 14/11/24.
+//  Created by Isabel Vaca on 07/11/24.
 //
 
 
 #include <iostream>
 #include "Estanteria.h"
 #include "Usuario.h"
-#include "Estanteria personalizada.h"
+#include "Libro.h"
+#include "Libro_digital.h"
+#include "Libro_fisico.h"
 using namespace std;
 
 
 int main() {
-    
-    //objeto 1
-    Libro libro1("The poppy war", "Fantasy");
-    Libro libro2("Pride and Prejudice", "Romance");
-    
-    //objeto 2
-    Estanteria estanteria_Juan("Estanteria de Juan");
-    
-    string titulo_libro = libro1.get_titulo();
-    
-    estanteria_Juan.agrega_libro(titulo_libro);
-    
-    estanteria_Juan.mostrar_estanteria();
-    
-    //objeto 3
-    Estanteria estanteria_Isa("Estantería de Isa");
-    
-    string pride_Prejudice = libro2.get_titulo();
-    
-    estanteria_Isa.agrega_libro(pride_Prejudice);
-    
-    estanteria_Isa.agrega_libro(titulo_libro);
-    
-    string nombre_est_Isa = estanteria_Isa.get_nombre();
-    
-    //objeto 4 estanteria
-    Estanteria estanteria_prueba("Pruebita lol");
-    
-    estanteria_prueba.agrega_libro(pride_Prejudice);
-    estanteria_prueba.agrega_libro(titulo_libro);
-    string ejemplo_estanteria_prueba = estanteria_prueba.get_coleccion_libros();
-    estanteria_prueba.mostrar_estanteria();
-    
-    //objeto 3 usuario
+    //libros del usuario
+    Usuario usuario("Isa");
+    Libro libro1("El Hobbit", "Fantasía", "J.R.R. Tolkien");
 
-    Usuario Usuario_Isa("Isa", nombre_est_Isa, ejemplo_estanteria_prueba );
-    Usuario_Isa.mostrar_estanteria();
+    usuario.agregar_Libro(libro1, "leido");
+    usuario.mostrar_estanteria();
+
+    //libro digital
+    Libro_digital libro_digital("1984", "Distopía", "George Orwell", 2.5);
+
+    // Mostrar información del libro digital
+    libro_digital.mostrar_libro_digital();
+    Libro_fisico the_PW("The Poppy War", "Fantasia militar", "R.F. KUANG", 600, "Nuevo");
+    //Se ingresa el tiempo promedio de lectura por pagina en minutos en el parametro
+    float tiempo_lectura = the_PW.calcular_tiempo_lectura(2);
+    cout << "Tiempo de lectura de: " << the_PW.get_titulo() << " es " << tiempo_lectura << " minutos." << endl;
+
     
-    
-    //objeto 4 estanteria personalizada
-    EstanteriaPersonalizada estanteria1("Estantería de Juan");
-    estanteria1.agrega_libro_leido(titulo_libro);
-    estanteria1.agrega_libro_leyendo("Harry Potter");
-    estanteria1.mostrar_estanteria();
+    usuario.agregar_Libro(the_PW, "leido");
+    usuario.agregar_Libro(libro_digital, "en lectura");
+
+    // Mostrar la estantería del usuario
+    usuario.mostrar_estanteria();
+
     
     return 0;
-    
-    
 }
