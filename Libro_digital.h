f1cbe2ac52361eb7b19441c559d6d8240504f433
Isabel Vaca <113130012+IsabelVaca@users.noreply.github.com>
2024-11-26 04:53:02 -0600

    Add files via upload

--- Libro_digital.h
+++ Libro_digital.h
@@ -1,32 +1,40 @@
 //
 //  Libro_digital.h
-//  Biblioteca personal
+//  Biblioteca personal corregido lol
 //
-//  Created by Isabel Vaca on 22/11/24.
+//  Created by Isabel Vaca on 26/11/24.
 //
+#ifndef LIBRO_DIGITAL_H
+#define LIBRO_DIGITAL_H
+
 #include "Libro.h"
-#include <iostream>
-#ifndef LIBRO_DIGITAL
-#define LIBRO_DIGITAL
-using namespace std;
-class Libro_digital: public Libro{
+//Clase libro digital, hereda de Libro
+class Libro_digital : public Libro {
+    //Atributo
 private:
-    float tamano_archivo;//En mb
-    
+    float tamano_archivo; // Tamaño del archivo en MB
+
 public:
-    Libro_digital(string t, string g, string a, float tam):
-    Libro(t,g,a), tamano_archivo(tam){}
-    
-    float get_tamano_archivo(){
-        return tamano_archivo;
-    }
-    void set_tamano_archivo(float tam){
-        tamano_archivo = tam;
-    }
-    void mostrar_libro_digital() {
-           mostrar_libro();
-           cout << " Tamaño del archivo: " << tamano_archivo << " MB" << endl;
-       }
-    
+    //Constructor y valores default
+    Libro_digital() : Libro(), tamano_archivo(0.0) {}
+    Libro_digital(string t, string g, string a, string e, float tam) : Libro(t, g, a, e), tamano_archivo(tam) {}
+//Getters, setters y metodos
+    float get_tamano_archivo();
+    void set_tamano_archivo(float tam);
+    void mostrar_libro_digital();
 };
+
+float Libro_digital::get_tamano_archivo() {
+    return tamano_archivo;
+}
+
+void Libro_digital::set_tamano_archivo(float tam) {
+    tamano_archivo = tam;
+}
+//Muestra info del libro digital
+void Libro_digital::mostrar_libro_digital() {
+    mostrar_libro();
+    cout << "Tamaño del archivo: " << tamano_archivo << " MB" << endl;
+}
+
 #endif
