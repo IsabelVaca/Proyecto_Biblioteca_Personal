d08dec9b7779068e8d4eff6ab63a9301b904c8c8
Isabel Vaca <113130012+IsabelVaca@users.noreply.github.com>
2024-11-26 18:04:31 -0600

    Add files via upload

--- Libro_digital.h
+++ Libro_digital.h
@@ -1,22 +1,31 @@
-//
-//  Libro_digital.h
-//  Biblioteca personal corregido lol
-//
-//  Created by Isabel Vaca on 26/11/24.
-//
+/*
+ *
+ * Biblioteca personal
+ *Isabel Vaca Sánchez
+ *A01352522
+ *
+ */
+
+/*
+ *Clase Libro digital que crea objetos de tipo libro digital, clase hija de Clase Libro.
+ *Tiene sus atributos especializados como el tamano del archivo.
+ *Tiene metodos getters, setters y para mostrar la info del objeto
+ *
+ */
 #ifndef LIBRO_DIGITAL_H
 #define LIBRO_DIGITAL_H
 
 #include "Libro.h"
-//Clase libro digital, hereda de Libro
+//Declaracion de la clase Libro_digital que hereda de la clase Libro
 class Libro_digital : public Libro {
-    //Atributo
+    //Atributos privados
 private:
-    float tamano_archivo; // Tamaño del archivo en MB
+    float tamano_archivo; // Tamano del archivo en MB
 
 public:
     //Constructor y valores default
     Libro_digital() : Libro(), tamano_archivo(0.0) {}
+    //Constructor que recibe valores para llenar variables de la instancia
     Libro_digital(string t, string g, string a, string e, float tam) : Libro(t, g, a, e), tamano_archivo(tam) {}
 //Getters, setters y metodos
     float get_tamano_archivo();
@@ -24,14 +33,27 @@ public:
     void mostrar_libro_digital();
 };
 
+/*
+/getter tamano archivo
+/@param:
+/@return: float
+*/
 float Libro_digital::get_tamano_archivo() {
     return tamano_archivo;
 }
-
+/*
+ *setter tamano archivo
+ *@param: float:tam
+ *@return:
+ */
 void Libro_digital::set_tamano_archivo(float tam) {
     tamano_archivo = tam;
 }
-//Muestra info del libro digital
+/*
+ *Muestra la informacion del libro
+ *@param: 
+ *@return:
+ */
 void Libro_digital::mostrar_libro_digital() {
     mostrar_libro();
     cout << "Tamaño del archivo: " << tamano_archivo << " MB" << endl;
