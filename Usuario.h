8b0390ee8927e786733c8dfee6bc8039688d2ce5
Isabel Vaca <113130012+IsabelVaca@users.noreply.github.com>
2024-11-26 18:05:17 -0600

    Add files via upload

--- Usuario.h
+++ Usuario.h
@@ -1,9 +1,19 @@
-//
-//  Usuario.h
-//  Biblioteca personal corregido lol
-//
-//  Created by Isabel Vaca on 26/11/24.
-//
+/*
+ *
+ * Biblioteca personal
+ *Isabel Vaca Sánchez
+ *A01352522
+ *
+ */
+
+
+/*
+ *Clase  Usuario que seria pues el usuario, este puede anadir libros fisicos y digitales a su estanteria.
+ *Tiene metodos getters, setters, para agregar libros a la estanteria, para mostrar la estanteria con
+ *todos los libros. Esta clase tiene composicion con los tipos de libros y con estanteria, ya que si se
+ *elimina la clase también lo hacen los libros y la estanteria que fueron creados desde esta clase.
+ *
+ */
 #ifndef USUARIO_H
 #define USUARIO_H
 
@@ -13,16 +23,17 @@
 #include "Estanteria.h"
 
 using namespace std;
-//Clase Usuario
+//Declaracion de la clase Usuario
 class Usuario {
-    //Atributos
+    //Declaracion de atributos
 private:
     string nombre_usuario;
     Estanteria estanteria;
 
 public:
-    //Constructor
+    //Constructor por default
     Usuario() : nombre_usuario("") {}
+    //Constructor que recibe valores para llenar variables de la instancia
     Usuario(string n) : nombre_usuario(n), estanteria() {}
 
     //Getters,setters y metodos
@@ -36,30 +47,60 @@ public:
            return estanteria;
        }
 };
-
+/*
+ *getter nombre usuario
+ *@param:
+ *@return: string: nombre_usuario
+ *
+ */
 string Usuario::get_nombre_usuario() {
     return nombre_usuario;
 }
-
+/*
+ *setter nombre usuario
+ *@param: string:n
+ *@return:
+ *
+ */
 void Usuario::set_nombre_usuario(string n) {
     nombre_usuario = n;
 }
-//Agrega libro fisico a la estanteria del usuario
+/*
+ *agrega libro fisico a la estanteria
+ *@param:string t, string g, string a, string e, int num_pag, string c
+ *@return:
+ *
+ */
 void Usuario::agregar_libro_fisico(string t, string g, string a, string e, int num_pag, string c) {
     Libro_fisico libro(t, g, a, e, num_pag, c);
     estanteria.agrega_libro_fisi(libro);
 }
-//Agrega libro digital a la estanteria del usuario
+/*
+ *agrega libro digital a la estanteria
+ *@param:string t, string g, string a, string e, float tam.
+ *@return:
+ *
+ */
 void Usuario::agregar_libro_digital(string t, string g, string a, string e, float tam) {
     Libro_digital libro(t, g, a, e, tam);
     estanteria.agrega_libro_dig(libro);
 }
-//Muestra la estanteria completa
+/*
+ *Muestra la estanteria
+ *@param:
+ *@return:
+ *
+ */
 void Usuario::mostrar_estanteria() {
     cout << "Estanteria de: " << nombre_usuario << ":\n";
     estanteria.mostrar_estanteria();
 }
-//Calcula el tiempo de lectura solo de libros fisicos
+/*
+ *Calcula tiempo de lectura de libro fisico
+ *@param:string t, int minutos_por_pagina
+ *@return: float
+ *
+ */
 float Usuario::calcular_tiempo_lib_fisi(string t, int minutos_por_pagina) {
     Libro_fisico libro = estanteria.buscar_lib_fisi(t);
     return libro.calcular_tiempo_lectura(minutos_por_pagina);
